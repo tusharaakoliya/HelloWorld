b942b15babc8c2376e07a55db81f0097eb2faf6e
tusharaakoliya <tusharaakoliya001@gmail.com>
2021-01-20 17:53:17 -0500

    worm up exercise 2

--- src/WarmUpExercises.java
+++ src/WarmUpExercises.java
@@ -2,5 +2,11 @@ public class WarmUpExercises {
     public static void main(String[] args) {
         for (int i=100;i>=0;i-=5)
             System.out.println(i);
+
+        // challenge 2 & 3
+        for (int i=0;i<=100;i +=10)
+        {
+            System.out.println(i%20 == 0 ? i + " Java":i);
+        }
     }
 }
